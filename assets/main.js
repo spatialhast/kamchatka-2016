650da39533192d4cb537e8b9b9616177bcd09f27
spatialhast <spatial.hast@gmail.com>
2016-10-19 13:38:06 +0300

    fix url

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,23 @@
-var layerTopomapper = new L.tileLayer('http://144.76.234.107/cgi-bin/ta/tilecache.py/1.0.0/topomapper_v2/{z}/{x}/{y}.jpg', {
+var layerTopomapper = new L.tileLayer('//144.76.234.107/cgi-bin/ta/tilecache.py/1.0.0/topomapper_v2/{z}/{x}/{y}.jpg', {
     maxNativeZoom: 13,
     attribution: 'Tiles: &copy; <a href="http://nakarte.tk/" target="_blank">nakarte.tk</a>'
 });
 
-var layerOpenTopoMap = new L.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+var layerOpenTopoMap = new L.TileLayer('//{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 		minZoom: 1,
 		maxZoom: 17,
 		detectRetina: true,
 		attribution: 'Kartendaten: &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende, <a href="http://viewfinderpanoramas.org">SRTM</a> | Kartendarstellung: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 	});
 
-var layerOpenCycleMap = new L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+var layerOpenCycleMap = new L.tileLayer('//{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>, under ODbL | Tiles: &copy; ' + '<a href="http://www.opencyclemap.org/" target="_blank">OpenCycleMap</a>'
 });
 
 
 
-       var layerMapboxImagery = new L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGFzdCIsImEiOiJjaXU5dWY2ZmowMDAwMnltZGpudHljYWg2In0.9ZcYfFSe65DvQq6qkYcmWg', {
+       var layerMapboxImagery = new L.tileLayer('//api.tiles.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGFzdCIsImEiOiJjaXU5dWY2ZmowMDAwMnltZGpudHljYWg2In0.9ZcYfFSe65DvQq6qkYcmWg', {
             maxZoom: 17,                          
             attribution: 'Tiles &copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a>'
         });
@@ -48,7 +48,7 @@ var BingLayer = L.TileLayer.extend({
         return quadKey.join('');
     }
 });
-var layerBingAerial = new BingLayer('http://t{s}.tiles.virtualearth.net/tiles/a{q}.jpeg?g=2732', {
+var layerBingAerial = new BingLayer('//t{s}.tiles.virtualearth.net/tiles/a{q}.jpeg?g=2732', {
     subdomains: ['0', '1', '2', '3', '4'],
     attribution: '&copy; <a href="http://bing.com/maps">Bing Maps</a>'
 });
@@ -56,7 +56,7 @@ var layerBingAerial = new BingLayer('http://t{s}.tiles.virtualearth.net/tiles/a{
         
         
         
-var layerTopo1000 = new L.tileLayer('http://{s}.tiles.nakarte.tk/topo1000/{z}/{x}/{y}', {
+var layerTopo1000 = new L.tileLayer('//{s}.tiles.nakarte.tk/topo1000/{z}/{x}/{y}', {
   tms: true,
   maxNativeZoom: 13,
   attribution: 'Tiles: &copy; <a href="http://nakarte.tk/" target="_blank">nakarte.tk</a>'
